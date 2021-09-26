5abe17e05076c21e523ea8ea2f4a89a7ab7bc189
LLH07 <lunghung.llh@gmail.com>
2021-09-26 11:18:47 +0800

    2021.09.26 Update: 終於解決加入時區問題，並更新取得 candles 的方法

--- oanda_api.py
+++ oanda_api.py
@@ -33,15 +33,23 @@ class OandaAPI():
         else:
             return None
 
-    def fetch_candles(self, pair_name, count, granularity):
+    def fetch_candles(self, pair_name, count=None, granularity='H1', date_from=None, date_to=None):
         # refer to save_candles.ipynb
         url = f'{defs.OANDA_URL}instruments/{pair_name}/candles'
 
-        params = dict(
-            count = count, 
+        params = dict( 
             granularity = granularity,
             price = 'MBA' # M: midpoint, B: bid, A: ask
         )
+
+        #20210926: 提供更詳細的所需資料
+        if date_from is not None and date_to is not None:
+            params['from'] = int(date_from.timestamp())
+            params['to'] = int(date_to.timestamp())
+        elif count is not None: # 不以時間段取 candle，而是直接給總共需要多少根 candles
+            params['count'] = count
+        else: # 不給時間也不給目標 candles 數，就直接取 300 根 candle
+            params['count'] = 300 
         
         response = self.session.get(url, params=params, headers=defs.SECURE_HEADER)
         
@@ -54,4 +62,10 @@ class OandaAPI():
 
 if __name__ == '__main__':
     api = OandaAPI()
-    api.save_instruments()
+    #api.save_instruments()
+    date_from = utils.get_utc_dt_from_string("2021-09-09 00:00:00")
+    date_to = utils.get_utc_dt_from_string("2021-09-15 00:00:00")
+
+    print(date_from)
+    print(api.fetch_candles('EUR_USD', date_from=date_from, date_to=date_to))
+    
--- utils.py
+++ utils.py
@@ -2,24 +2,28 @@
 import datetime as dt
 from dateutil.parser import *
 
+import pytz
+
 def get_his_data_filename(pair, granularity):
     return f'his_data\{pair}_{granularity}.pkl'
 
 def get_instruments_data_filename():
     return 'instruments.pkl'
 
-# 2021.09.21 取得目前 utc 時間並在其後增加 utc 時區
+# 2021.09.26 取得目前 utc 時間並在其後增加 utc 時區 (轉換為 OANDA 接受的格式)
 def time_utc():
-    return dt.datetime.utcnow().replace(tzinfo=dt.timezone.utc)
+    return dt.datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(pytz.timezone("Asia/Taipei"))
 
-# 2021.09.21 將字串轉為時間 datetime 
+# 2021.09.26 將字串轉為時間 datetime 
 def get_utc_dt_from_string(data_str):
     d = parse(data_str)
-    return d.replace(tzinfo=dt.timezone.utc)
+    return d.astimezone(pytz.timezone("Asia/Taipei")) # 這裡不用加 .replace()，因為字串是我們自己打入的
 
 if __name__ == "__main__":
     print(get_his_data_filename("EUR_USD", "H1"))
     print(get_instruments_data_filename())
+
     print(dt.datetime.utcnow())
     print(time_utc())
-    print(get_utc_dt_from_string('2021-09-21 20:00:00'))
+    print(get_utc_dt_from_string("2021-03-01 03:00:00"))
+    
